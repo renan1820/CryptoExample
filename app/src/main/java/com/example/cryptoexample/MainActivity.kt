9db50524fb2e008f5fd3b08bc91a57a5045ecac2
Renan Castro <renancas_ribeiro@hotmail.com.br>
2023-09-20 19:19:41 -0300

    Adjust View

--- app/src/main/java/com/example/cryptoexample/MainActivity.kt
+++ app/src/main/java/com/example/cryptoexample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cryptoexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -11,21 +12,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cryptoexample.ui.theme.CryptoExampleTheme
+import kotlinx.coroutines.launch
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -42,9 +50,17 @@ class MainActivity : ComponentActivity() {
                 var messageToEncrypt by remember {
                     mutableStateOf("")
                 }
-                var messageToDencrypt by remember {
+                var messageToDecrypt by remember {
                     mutableStateOf("")
                 }
+
+                var result by remember {
+                    mutableStateOf(messageToDecrypt)
+                }
+
+                val scope = rememberCoroutineScope()
+                val snackbarHostState = remember { SnackbarHostState() }
+
 
                 Column (
                     modifier = Modifier
@@ -62,18 +78,22 @@ class MainActivity : ComponentActivity() {
 
                     Row {
                         Button(onClick = {
-                            val bytes = messageToEncrypt.encodeToByteArray()
-                            val file = File(filesDir, "secret.txt")
-                            if(!file.exists()){
-                                file.createNewFile()
+                            messageToEncrypt.let {
+                                val bytes = it.encodeToByteArray()
+                                val file = File(filesDir, "secret.txt")
+                                if(!file.exists()){
+                                    file.createNewFile()
+                                }
+                                val fos = FileOutputStream(file)
+
+                                messageToDecrypt = cryptoManager.encrypt(
+                                    bytes = bytes,
+                                    outputStream = fos
+                                ).decodeToString()
+                            }.also {
+
+
                             }
-                            val fos = FileOutputStream(file)
-
-                            messageToDencrypt = cryptoManager.encrypt(
-                                bytes = bytes,
-                                outputStream = fos
-                            ).decodeToString()
-
                         }) {
                             Text(text = "Encrypt")
                         }
@@ -81,11 +101,39 @@ class MainActivity : ComponentActivity() {
                         Button(onClick = {
                             val file = File(filesDir,"secret.txt")
                             messageToEncrypt = cryptoManager.decrypt(inputStream = FileInputStream(file)).decodeToString()
+                            result = messageToEncrypt
+
                         }) {
                             Text(text = "Decrypt")
                         }
                     }
-                    Text(text = messageToDencrypt)
+
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = messageToDecrypt,
+                        onValueChange = {},
+                        enabled = false,
+                        label = {
+                            Text( text = "Encrypted")
+                        },
+                        singleLine = true,
+                        maxLines = 1
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = result,
+                        onValueChange = {},
+                        enabled = false,
+                        label = {
+                            Text(text = "Decrypted"
+                            )
+                        },
+                        singleLine = true,
+                    )
+
                 }
             }
         }
